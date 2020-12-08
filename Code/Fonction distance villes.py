ca1abc0d8c961d213e7d68112452225f8f762c55
Matthieu <mattsim77@orange.fr>
2020-12-08 18:37:41 +0100

    MAJ

--- Code/Fonction distance villes.py
+++ Code/Fonction distance villes.py
@@ -9,34 +9,11 @@ https://geodesie.ign.fr/contenu/fichiers/Distance_longitude_latitude.pdf
 from math import sin, cos, acos, pi
  
 #############################################################################
-def dms2dd(d, m, s):
-    """Convertit un angle "degrés minutes secondes" en "degrés décimaux"
-    """
-    return d + m/60 + s/3600
- 
-#############################################################################
-def dd2dms(dd):
-    """Convertit un angle "degrés décimaux" en "degrés minutes secondes"
-    """
-    d = int(dd)
-    x = (dd-d)*60
-    m = int(x)
-    s = (x-m)*60
-    return d, m, s
- 
-#############################################################################
 def deg2rad(dd):
     """Convertit un angle "degrés décimaux" en "radians"
     """
     return dd/180*pi
- 
-#############################################################################
-def rad2deg(rd):
-    """Convertit un angle "radians" en "degrés décimaux"
-    """
-    return rd/pi*180
- 
-#############################################################################
+ #############################################################################
 def distanceGPS(latA, longA, latB, longB):
     """Retourne la distance en mètres entre les 2 points A et B connus grâce à
        leurs coordonnées GPS (en radians).
